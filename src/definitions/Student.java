ac7f4def62aa9a4fd2de8710aca4e3f8ac8b4ed4
Pachauri <prajjwalpachauri725@gmail.com>
2020-09-10 20:41:17 +0530

    add getFullName and setFullName in Student class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -80,4 +80,10 @@ public class Student {
     public void setLastName(String lastName) {
        this.lastName = lastName;
     }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
