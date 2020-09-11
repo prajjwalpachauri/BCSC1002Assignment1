404cc39d3b5ac28e8e4cc01804120673f3bc9fab
Pachauri <prajjwalpachauri725@gmail.com>
2020-09-11 20:29:13 +0530

    add all the necessary override methods from Object class in Book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
     private String name;
     private String authorName;
@@ -16,22 +18,55 @@ public class Book {
         this.authorName = authorName;
         this.isbn = isbn;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getAuthorName() {
         return authorName;
     }
+
     public void setAuthorName(String authorName) {
         this.authorName = authorName;
     }
+
     public String getIsbn() {
         return isbn;
     }
+
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return Objects.equals(getName(), book.getName()) &&
+                Objects.equals(getAuthorName(), book.getAuthorName()) &&
+                Objects.equals(getIsbn(), book.getIsbn());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAuthorName(), getIsbn());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Book Name: '%s'.\n +" +
+                        "Author Name: '%s'.\n" +
+                        "ISBN Number: '%s'.",
+                this.getName(), this.getAuthorName(), this.getIsbn());
     }
 }
